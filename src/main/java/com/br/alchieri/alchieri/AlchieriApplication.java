c3df7bdb9bdf994fc2011a52813a00b4bb76260b
Alchieri <alchierialchieri@gmail.com>
2024-10-30 15:56:39 -0300

    inicial

---
+++ src/main/java/com/br/alchieri/alchieri/AlchieriApplication.java
@@ -0,0 +1,13 @@
+package com.br.alchieri.alchieri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlchieriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlchieriApplication.class, args);
+	}
+
+}
